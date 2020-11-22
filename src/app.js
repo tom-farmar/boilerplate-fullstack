d0059fd4cbe3b49239ef3cf27216a67227c97953
Tom Farmar <tom@signedoff.com>
2020-11-22 11:47:05 -0500

    incorporate react-spring

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { AuthenticatedApp } from './authenticated-app'
 import { UnauthenticatedApp } from './unauthenticated-app'
 
 export function App() {
-  const user = false
+  const user = true
   return (
     <React.Suspense fallback={<div>Loading...</div>}>
       {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
